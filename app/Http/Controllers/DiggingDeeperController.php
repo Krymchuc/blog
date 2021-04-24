322be8f12f161f091d596ffe9c4d3fb294200c1a
Krymchuc <khrystyna.krymchuk@oa.edu.ua>
2021-04-24 23:37:32 +0300

    lab №13

--- app/Http/Controllers/DiggingDeeperController.php
+++ app/Http/Controllers/DiggingDeeperController.php
@@ -1,12 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Jobs\ProcessVideoJob;
+use App\Jobs\GenerateCatalog\GenerateCatalogMainJob;
 use App\Models\BlogPost;
 use Carbon\Carbon;
 
 class DiggingDeeperController extends Controller
 {
+    public function processVideo()
+    {
+        ProcessVideoJob::dispatch();
+        // Відкладення виконання завдання від моменту потрапляння в чергу.
+        // Не впливає на паузу між спробами виконання завдання.
+        //->delay(10)
+        //->onQueue('name_of_queue')
+    }
+
+    /**
+     * @link http://blog.com/digging_deeper/prepare-catalog
+     *
+     * php artisan queue:listen --queue=generate-catalog --tries=3 --delay=10
+     */
+    public function prepareCatalog()
+    {
+        GenerateCatalogMainJob::dispatch();
+    }
     /**
      * Базова інформація
      * @url https://laravel.com/docs/8.x/collections#introduction
@@ -125,4 +144,6 @@ class DiggingDeeperController extends Controller
         //dd(compact('sortedSimpleCollection', 'sortedAscCollection', 'sortedDescCollection'));
 
     }
+
+
 }
